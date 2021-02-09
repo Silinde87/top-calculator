574ebff08f62dbabc0275ce908275e92f4911236
Silinde87 <paurodriguezmolina@gmail.com>
2021-02-09 10:38:04 +0100

    feat: Implement operations functions

---
+++ operations.js
@@ -0,0 +1,33 @@
+//Basic math operations
+function add(a,b){
+    return a+b;
+}
+function subtract(a,b){
+    return a-b;
+}
+function multiply(a,b){
+    return a*b;
+}
+function divide(a,b){
+    return a/b;
+}
+
+//This function selects the operation based on the operator parameter
+function operate(operator, a, b){
+    let result;
+    switch(operator){
+        case '+':
+            result = add(a,b);
+            break;
+        case '-':
+            result = subtract(a,b);
+            break;
+        case 'x':
+            result = multiply(a,b);
+            break;
+        case '÷':
+            result = divide(a,b);
+            break;
+    }
+    return result;
+}
